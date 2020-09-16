adc954c8716a937f4de796598fb4361e6f4aa756
williamsimms <williamsimms193@gmail.com>
2020-09-16 15:57:35 -0400

    added feed component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.scss'
 import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <div className='app__body'>
         <Sidebar />
+        <Feed />
       </div>
     </div>
   )
