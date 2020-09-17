29cbaf8b6138511b492ed18cd71e41560ebd48a5
williamsimms <williamsimms193@gmail.com>
2020-09-17 14:49:22 -0400

    styled stories

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -5,7 +5,31 @@ import Story from './Story'
 export default function Stories() {
   return (
     <div className='stories'>
-      <Story />
+      <Story
+        image='https://images.unsplash.com/photo-1539947257641-0f0e9f213528?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        profileImage='https://pbs.twimg.com/profile_images/1277454851195179009/-p_CRLYU_400x400.jpg'
+        title='William Simms'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1539947257641-0f0e9f213528?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        profileImage='https://pbs.twimg.com/profile_images/1277454851195179009/-p_CRLYU_400x400.jpg'
+        title='William Simms'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1539947257641-0f0e9f213528?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        profileImage='https://pbs.twimg.com/profile_images/1277454851195179009/-p_CRLYU_400x400.jpg'
+        title='William Simms'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1539947257641-0f0e9f213528?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        profileImage='https://pbs.twimg.com/profile_images/1277454851195179009/-p_CRLYU_400x400.jpg'
+        title='William Simms'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1539947257641-0f0e9f213528?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        profileImage='https://pbs.twimg.com/profile_images/1277454851195179009/-p_CRLYU_400x400.jpg'
+        title='William Simms'
+      />
     </div>
   )
 }
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
 import './Story.scss'
 
-export default function Story() {
-  return <div className='story'></div>
+export default function Story({ image, profileImage, title }) {
+  return (
+    <div
+      className='story'
+      style={{
+        backgroundImage: `url(${image})`,
+      }}>
+      <Avatar src={profileImage} className='story__avatar' />
+      <h4 className='story__title'>{title}</h4>
+    </div>
+  )
 }
