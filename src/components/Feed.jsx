f4d4ad429c941613182c6695eb1e96717c152b3a
williamsimms <williamsimms193@gmail.com>
2020-09-17 15:31:48 -0400

    added post sender

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Feed.scss'
 import Stories from './Stories'
+import PostSender from './PostSender'
 
 export default function Feed() {
   return (
     <div className='feed'>
       <Stories />
+      <PostSender />
     </div>
   )
 }
---
+++ src/components/PostSender.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Avatar } from '@material-ui/core'
+import VideocamIcon from '@material-ui/icons/Videocam'
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import './PostSender.scss'
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+}
+
+export default function PostSender() {
+  return (
+    <div className='postSender'>
+      <div className='postSender__top'>
+        <Avatar />
+        <form>
+          <input type='text' placeholder='Whats on your mind' className='postSenderinput' />
+          <input type='text' placeholder='Image URL (Optional)' className='postSenderImageinput' />
+          <button onClick={handleSubmit} type='submit'>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className='postSender__bottom'>
+        <div className='postSender__bottom__option'>
+          <VideocamIcon className='postSender__bottom__option__icon' style={{ color: 'red' }} />
+          <h3>Live Video</h3>
+        </div>
+
+        <div className='postSender__bottom__option'>
+          <PhotoLibraryIcon className='postSender__bottom__option__icon' style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
+
+        <div className='postSender__bottom__option'>
+          <InsertEmoticonIcon className='postSender__bottom__option__icon' style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
