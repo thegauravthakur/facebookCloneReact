82f572aedb067b85958723d92e63dc3f8e380f51
williamsimms <williamsimms193@gmail.com>
2020-09-18 11:27:02 -0400

    added reducer

---
+++ src/StateProvider.jsx
@@ -0,0 +1,10 @@
+import { FiberPin } from '@material-ui/icons'
+import React, { createContext, useReducer, useContext } from 'react'
+
+export const StateContext = createContext()
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>{children}</StateContext.Provider>
+)
+
+export const useStateValue = () => useContext(StateContext)
