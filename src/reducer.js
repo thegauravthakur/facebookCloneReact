b22e3e518728bd2496981ad9bac20c494504f299
williamsimms <williamsimms193@gmail.com>
2020-09-18 11:29:37 -0400

    added action types

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,21 @@
-export const reducer = (state, action) => {
+export const initialState = {
+  user: null,
+}
+
+export const actionTypes = {
+  SET_USER: 'SET_USER',
+}
+
+const reducer = (state, action) => {
   switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      }
     default:
       return state
   }
 }
+
+export default reducer
