12383d21bd165232d2151dea054009764fb9eaa4
williamsimms <williamsimms193@gmail.com>
2020-09-20 11:43:25 -0400

    message array looped through

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -3,10 +3,21 @@ import './Messenger.scss'
 
 export default function Messenger() {
   const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const sendMessage = (e) => {
+    setMessages([...messages, input])
+
+    setInput('')
+  }
+
   return (
     <div className='messenger'>
       <input type='text' onChange={(e) => setInput(e.target.value)} value={input} />
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
+      {messages.map((message) => (
+        <p>message</p>
+      ))}
     </div>
   )
 }
