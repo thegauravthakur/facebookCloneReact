06f5338a76b2c5a643cd4283b534faeb2c69f9f6
williamsimms <williamsimms193@gmail.com>
2020-09-25 16:49:48 -0400

    fixes

--- src/components/ErrorNavbar.jsx
+++ src/components/ErrorNavbar.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 import './ErrorNavbar.scss'
+import facebookIcon from '../img/facebook.png'
 
 export default function ErrorNavbar() {
   return (
     <div className='errorNavbar'>
       <div className='errorNavbar__container'>
         <div className='errorNavbar__logo'>
-          <img src='' alt='Facebook Logo' />
+          <img className='errorNavbar__logo__img' src={facebookIcon} alt='Facebook Logo' />
           <Link to='/home'>Sign Up</Link>
         </div>
 
