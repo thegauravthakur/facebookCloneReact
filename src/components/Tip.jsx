5e3c5819711516c23af1b4f554a9bea55f878a9b
williamsimms <williamsimms193@gmail.com>
2020-09-29 15:02:48 -0400

    fixes

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Tip.scss'
 
-function Tip({ text }) {
+function Tip({
+  text,
+  homeHover,
+  friendsHover,
+  watchHover,
+  groupsHover,
+  gamingHover,
+  createHover,
+  messengerHover,
+  notificationsHover,
+  accountHover,
+}) {
   return (
-    <div className='tip'>
+    <div
+      className={`tip ${homeHover && 'tip__home'} ${friendsHover && 'tip__friends'} ${watchHover && 'tip__watch'} ${
+        groupsHover && 'tip__groups'
+      } ${gamingHover && 'tip__gaming'} ${createHover && 'tip__create'} ${messengerHover && 'tip__messenger'} ${
+        notificationsHover && 'tip__notifications'
+      } ${accountHover && 'tip__account'}`}>
       <p>{text}</p>
     </div>
   )
