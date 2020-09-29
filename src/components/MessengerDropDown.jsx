88f9f849cde2813aa5c15b11f8c794a81c628c0e
williamsimms <williamsimms193@gmail.com>
2020-09-29 19:12:15 -0400

    fixes

--- src/components/MessengerDropDown.jsx
+++ src/components/MessengerDropDown.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import VideoCallIcon from '@material-ui/icons/VideoCall'
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit'
 import CreateIcon from '@material-ui/icons/Create'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import SearchIcon from '@material-ui/icons/Search'
 import Tip from './Tip'
+import MessengerDropDownUser from './MessengerDropDownUser'
 import './MessengerDropDown.scss'
 
 function MessengerDropDown() {
@@ -13,8 +15,19 @@ function MessengerDropDown() {
       <div className='messengerDropDown__container'>
         <div className='messengerDropDown__header'>
           <h3>Messenger</h3>
+
+          <div className='messengerDropDown__header__icon__container'>
+            <FullscreenExitIcon className='messengerDropDown__header__icon' />
+            <VideoCallIcon className='messengerDropDown__header__icon' />
+            <CreateIcon className='messengerDropDown__header__icon' />
+            <MoreHorizIcon className='messengerDropDown__header__icon' />
+          </div>
         </div>
-        <FullscreenExitIcon className='messengerDropDown__header__icon' />
+
+        <div className='messengerDropDown__search'>
+          <SearchIcon className='messengerDropDown__search__icon' />
+          <input type='text' className='messengerDropDown__search__input' placeholder='Search Messenger' />
+        </div>
       </div>
     </div>
   )
---
+++ src/components/MessengerDropDownUser.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './MessengerDropDownUser.scss'
+
+function MessengerDropDownUser() {
+  return <div className='messengerDropDownUser'></div>
+}
+
+export default MessengerDropDownUser
