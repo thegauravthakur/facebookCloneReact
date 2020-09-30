b76387aaf9cc475377cc04487f682ce361a42b76
williamsimms <williamsimms193@gmail.com>
2020-09-30 11:36:49 -0400

    styling

---
+++ src/components/DropDownNotification.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import './DropDownNotification.scss'
+
+function DropDownNotification() {
+  return <div></div>
+}
+
+export default DropDownNotification
--- src/components/NotificationsDropDown.jsx
+++ src/components/NotificationsDropDown.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import Notification from './DropDownNotification'
+import CakeIcon from '@material-ui/icons/Cake'
+import GroupIcon from '@material-ui/icons/Group'
+import PostAddIcon from '@material-ui/icons/PostAdd'
 import './NotificationsDropDown.scss'
 
 function NotificationsDropDown() {
