bcf22b3d40a87e9c651de9bfd9790f214dbf6e9b
williamsimms <williamsimms193@gmail.com>
2020-09-29 20:31:35 -0400

    added tips to messenger dropdown

--- src/components/MessengerDropDown.jsx
+++ src/components/MessengerDropDown.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import onClickOutside from 'react-onclickoutside'
 import VideoCallIcon from '@material-ui/icons/VideoCall'
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit'
 import CreateIcon from '@material-ui/icons/Create'
@@ -10,6 +11,8 @@ import MessengerDropDownUser from './MessengerDropDownUser'
 import './MessengerDropDown.scss'
 
 function MessengerDropDown() {
+  const history = useHistory()
+
   return (
     <div className='messengerDropDown'>
       <div className='messengerDropDown__container'>
@@ -17,17 +20,22 @@ function MessengerDropDown() {
           <h4>Messenger</h4>
 
           <div className='messengerDropDown__header__icons'>
-            <div className='messengerDropDown__header__icon__container'>
+            <div className='messengerDropDown__header__icon__container' onClick={() => history.push('/messenger')}>
               <FullscreenExitIcon className='messengerDropDown__header__icon' />
+              <Tip text='See All in Messenger' />
             </div>
             <div className='messengerDropDown__header__icon__container'>
               <VideoCallIcon className='messengerDropDown__header__icon' />
+              <Tip text='Create new room' />
             </div>
             <div className='messengerDropDown__header__icon__container'>
               <CreateIcon className='messengerDropDown__header__icon' />
+
+              <Tip text='New message' />
             </div>
             <div className='messengerDropDown__header__icon__container'>
               <MoreHorizIcon className='messengerDropDown__header__icon' />
+              <Tip text='Options' />
             </div>
           </div>
         </div>
