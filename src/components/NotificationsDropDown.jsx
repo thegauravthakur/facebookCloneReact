d20e838b8b78928eda2a16be46bd2efc6e1681c2
williamsimms <williamsimms193@gmail.com>
2020-09-30 12:45:11 -0400

    styling for notifications dropdown

--- src/components/NotificationsDropDown.jsx
+++ src/components/NotificationsDropDown.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import onClickOutside from 'react-onclickoutside'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import Notification from './DropDownNotification'
 import CakeIcon from '@material-ui/icons/Cake'
 import GroupIcon from '@material-ui/icons/Group'
 import PostAddIcon from '@material-ui/icons/PostAdd'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import VideocamIcon from '@material-ui/icons/Videocam'
 import './NotificationsDropDown.scss'
 
-function NotificationsDropDown() {
+function NotificationsDropDown({ isDropDownOpen, setNotificationsDropDownOpen }) {
   return (
     <div className='notificationsDropDown'>
-      <div className='notificationsDropDown__container'></div>
+      <div className='notificationsDropDown__container'>
+        <div className='notificationsDropDown__header'>
+          <h4>Notifications</h4>
+          <div className='notificationsDropDown__header__icon__container'>
+            <MoreHorizIcon className='notificationsDropDown__header__icon' />
+          </div>
+        </div>
+
+        <h5>New</h5>
+      </div>
     </div>
   )
 }
