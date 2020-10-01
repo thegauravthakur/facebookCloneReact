bfa3f9cc69bd680531a271e048eefb716c636bba
williamsimms <williamsimms193@gmail.com>
2020-09-30 22:04:30 -0400

    added page components

---
+++ src/components/RoomCreator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddIcon from '@material-ui/icons/Add'
+import Tip from './Tip'
+import './RoomCreator.scss'
+
+function RoomCreator() {
+  return <div className='roomCreator'></div>
+}
+
+export default RoomCreator
